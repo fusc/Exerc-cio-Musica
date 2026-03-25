1b27bd5d5b3ae771bed76a287c0d6b374b8bba8c
DESKTOP-LN7SD4G\user <user>
2026-03-25 20:21:26 -0300

    Exercício Musica

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Tipo Leon S. Kennedy ️ (Resident Evil Requiem)");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Eles criaram monstros");
+Console.WriteLine("Esses demônios malditos");
+Console.WriteLine("Onde o infectado vira parte da orda");
+Console.WriteLine("Então não queira ser mordido");
+Console.WriteLine("Com essa mutação ");
+Console.WriteLine("O surto invade o indivíduo ");
+Console.WriteLine("O hospedeiro dessa maldição ");
+Console.WriteLine("Carrega uma infecção ");
+Console.WriteLine("Que te torna um parasita revivido");
+Console.ResetColor();
